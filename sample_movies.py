c28942879693a3cd1cccecdd06162f972645882b
sarthak1996 <khandekarsarthak@gmail.com>
2018-12-02 05:03:09 +0530

    Initial commit

---
+++ sample_movies.py
@@ -0,0 +1,12 @@
+import media
+import fresh_tomatoes
+pulp_fiction = media.Movie('Pulp Fiction', "In the realm of underworld, a series of incidents intertwines the lives of two Los Angeles mobsters, a gangster's wife, a boxer and two small-time criminals.", 'https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg', "https://www.youtube.com/watch?v=tGpTpVyI_OQ")
+
+deadpool = media.Movie('Deadpool', "Mercenary Wade Wilson, subjected to an experiment to heal himself of cancer, obtains healing powers, but at the cost of becoming awfully disfigured. He then adopts the alter ego of Deadpool.", 'http://www.gstatic.com/tv/thumb/v22vodart/11098044/p11098044_v_v8_af.jpg', 'https://www.youtube.com/watch?v=9vN6DHB6bJc')
+
+dark_knight = media.Movie('The Dark Knight', "With the help of allies Lt. Jim Gordon (Gary Oldman) and DA Harvey Dent (Aaron Eckhart), Batman (Christian Bale) has been able to keep a tight lid on crime in Gotham City. But when a vile young criminal calling himself the Joker (Heath Ledger) suddenly throws the town into chaos, the caped Crusader begins to tread a fine line between heroism and vigilantism.", 'https://i.kinja-img.com/gawker-media/image/upload/s--A1lC8jc---/c_scale,f_auto,fl_progressive,q_80,w_800/ya4c8rc9pwfcsn8awr1e.jpg', 'https://www.youtube.com/watch?v=EXeTwQWrcwY')
+
+avengers_infinity_war = media.Movie('Avengers: Infinity War', "Iron Man, Thor, the Hulk and the rest of the Avengers unite to battle their most powerful enemy yet -- the evil Thanos. On a mission to collect all six Infinity Stones, Thanos plans to use the artifacts to inflict his twisted will on reality. The fate of the planet and existence itself has never been more uncertain as everything the Avengers have fought for has led up to this moment.", 'http://www.gstatic.com/tv/thumb/v22vodart/12863030/p12863030_v_v8_ae.jpg', 'https://www.youtube.com/watch?v=6ZfuNTqbHE8')
+
+movies = [pulp_fiction, deadpool, dark_knight, avengers_infinity_war]
+fresh_tomatoes.open_movies_page(movies)

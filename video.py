f9900b5554080762acd5be0d9b916e6e27dd4086
sarthak1996 <khandekarsarthak@gmail.com>
2018-12-13 21:14:17 +0530

    Implementation changes for classes..Added methods to set attrbs

--- video.py
+++ video.py
@@ -1,39 +1,34 @@
+import webbrowser
+
+
 class Video():
     comments = None
+    attrs = {0: 'title', 1: 'storyline', 2: 'duration', 3: 'poster_image_url', 4: 'trailer_youtube_url'}
 
-    def __init__(self, title, storyline, duration=0):
+    '''
+    XML tags
+    '''
+    TITLE_TAG = '    <title>{content}</title>\n'
+    STORYLINE_TAG = '    <storyline>{content}</storyline>\n'
+    DURATION_TAG = '    <duration>{content}</duration>\n'
+    POSTER_URL_TAG = '    <poster>{content}</poster>\n'
+    TRAILER_TAG = '    <trailer>{content}</trailer>\n'
+    '''
+    '''
+
+    def __init__(self, title, storyline, poster, trailer, duration=0):
         self.title = title
         self.duration = duration
         self.storyline = storyline
+        self.poster_image_url = poster
+        self.trailer_youtube_url = trailer
 
     def __init__(self):
         self.title = None
         self.duration = 0
         self.storyline = None
-
-    def get_title(self):
-        return self.title
-
-    def get_duration(self):
-        return self.duration
-
-    def set_duration(self, duration):
-        self.duration = duration
-
-    def set_title(self, title):
-        self.title = title
-
-    def set_storyline(self, storyline):
-        self.storyline = storyline
-
-    def get_storyline(self):
-        return self.storyline
-
-    def get_comments(self):
-        return self.comments
-
-    def set_comments(self, comments):
-        self.comments = comments
+        self.poster_image_url = None
+        self.trailer_youtube_url = None
 
     def add_comment(self, comment):
         if self.comments is None:
@@ -43,8 +38,46 @@ class Video():
     def has_comments(self):
         return not(self.comments is None)
 
-    def convert_to_xml(self):
-        content = '  <title>' + self.title.replace('</', '').replace('>', '') + '</title>\n'
-        content += '  <storyline>' + self.storyline.replace('</', '').replace('>', '') + '</storyline>\n'
-        content += '  <duration>' + str(self.duration) + '</duration>\n'
+    def convert_to_xml(self, container_tag):
+        content = self.TITLE_TAG.format(content=self.title)
+        content += self.STORYLINE_TAG.format(content=self.storyline)
+        content += self.DURATION_TAG.format(content=self.duration)
+        content += self.POSTER_URL_TAG.format(content=self.poster_image_url)
+        content += self.TRAILER_TAG.format(content=self.trailer_youtube_url)
+        content = container_tag.format(content=content)
         return content
+
+    def get_attr(self):
+        return self.attrs
+
+    def set_attr_value(self, attr_index, value):
+        if attr_index == 0:
+            self.title = value
+        elif attr_index == 1:
+            self.storyline = value
+        elif attr_index == 2:
+            self.duration = value
+        elif attr_index == 3:
+            self.poster_image_url = value
+        elif attr_index == 4:
+            self.trailer_youtube_url = value
+        else:
+            print('Atrb exists but update method is not defined for it!')
+            return -1
+        return 1
+
+    def set_attr(self, attr_indices, values):
+        if len(attr_indices) != len(values):
+            print('ERROR: Length of indices and values do not match!')
+            return -1
+        for chosen_attr in attr_indices:
+            if chosen_attr in attrs:
+                if (self.set_attr_value(self, chosen_attr, values[chosen_attr])) == -1:
+                    return -1
+            else:
+                print('ERROR: Entered index is not a valid index for attribute!')
+                return -1
+        return self
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)

e062932ad05b7b1f90e7d1e1ea6aa745fc239cc1
sarthak1996 <khandekarsarthak@gmail.com>
2018-12-22 13:50:10 +0530

    Added clear function

--- media.py
+++ media.py
@@ -9,6 +9,7 @@ class Movie(Video):
 
     def __init__(self, movie_title=None, movie_storyline=None, movie_poster=None, movie_trailer=None, movie_duration=0):
         Video.__init__(self, movie_title, movie_storyline, movie_poster, movie_trailer, movie_duration)
+        self.type='M'
 
     def convert_to_xml(self):
         return Video.convert_to_xml(self, self.MOVIE_TAG)
@@ -22,6 +23,7 @@ class Series(Video):
 
     def __init__(self, series_title=None, series_storyline=None, series_poster=None, series_trailer=None, series_duration=0):
         Video.__init__(self, series_title, series_storyline, series_poster, series_trailer, series_duration)
+        self.type='S'
 
     def convert_to_xml(self):
         return Video.convert_to_xml(self, self.SERIES_TAG)
@@ -35,6 +37,7 @@ class Anime(Video):
 
     def __init__(self, anime_title=None, anime_storyline=None, anime_poster=None, anime_trailer=None, anime_duration=0):
         Video.__init__(self, anime_title, anime_storyline, anime_poster, anime_trailer, anime_duration)
+        self.type='A'
 
     def convert_to_xml(self):
         return Video.convert_to_xml(self, self.ANIME_TAG)
